ce074ab9066022b29a0345056d45a8f20138be87
Erman Louie Faminiano <119798034+ditosoft-erman@users.noreply.github.com>
2024-12-01 15:56:56 +0800

    Add files via upload

---
+++ client/types/navigation.d.ts
@@ -0,0 +1,16 @@
+// Types
+
+type AuthNavigationType = {
+  Welcome: undefined;
+  Login: undefined;
+  Register: undefined;
+  Splash: undefined;
+  Home: undefined;
+  User: undefined;
+  Code: undefined;
+  Reset: undefined;
+};
+
+type SplashNavigationType = {
+  Welcome: undefined;
+};
